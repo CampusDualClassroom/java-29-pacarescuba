f888a7ece133e64bf115fcc749d7e576276127cc
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2025-01-31 12:18:38 +0100

    Add solution for Exercise29

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,11 @@
 package com.campusdual.classroom;
 
 public class Exercise29 {
-    public static void main(String[] args) {
-
+  public static void main(String[] args) {
+    try {
+      Integer.parseInt("a");
+    } catch (NumberFormatException e) {
+      System.out.print("ERROR: La string no es un número");
     }
+  }
 }
